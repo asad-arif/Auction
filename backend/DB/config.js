ed2cf2433085bddbb9e137e37fa1883a93c762fa
mdotasad <asadofficial01@gmail.com>
2024-07-22 14:03:08 +0500

    db fixed

--- backend/DB/config.js
+++ backend/DB/config.js
@@ -1,14 +1,16 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const uri =
   "mongodb+srv://asad:asad@auction.pdi0nqu.mongodb.net/?retryWrites=true&w=majority&appName=Auction";
 
 mongoose
-  .connect(uri)
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("Connected to DB");
   })
   .catch((err) => {
-    console.error("Error connecting to DB:", err);
+    console.error("Error connecting to DB:");
   });
