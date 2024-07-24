8a9b45042005e0b2592e30c52d3692f49ef44226
mdotasad <asadofficial01@gmail.com>
2024-07-24 22:23:45 +0500

    backend fixed

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,48 +1,50 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:7000";
+// const baseUrl = process.env.REACT_APP_API_URL;
 
-export const signup = async (body: any) => {
-  const res = await axios.post(`${baseUrl}/signup`, body);
+export const signup = async (body: never) => {
+  const res = await axios.post(`${baseUrl}/users/signup`, body);
   return res;
 };
 
-export const login = async (body: any) => {
-  const res = await axios.post(`${baseUrl}/login`, body);
+export const login = async (body: never) => {
+  const res = await axios.post(`${baseUrl}/users/login`, body);
   return res;
 };
 
-export const createAuction = async (body: any) => {
+export const createAuction = async (body: never) => {
   const res = await axios.post(`${baseUrl}/create-auction`, body);
   return res;
 };
 
 export const getAllAuctions = async () => {
-  const res = await axios.get(`${baseUrl}/get-all-auctions`);
+  const res = await axios.get(`${baseUrl}/auctions/get-all-auctions`);
   return res;
 };
 
-export const getAuctionsByUserId = async (id: string) => {
-  const res = await axios.get(`${baseUrl}/get-auctions-by-userId/${id}`);
+export const getAuctionsByUserId = async (id: never) => {
+  const res = await axios.get(
+    `${baseUrl}/auctions/get-auctions-by-userId/${id}`
+  );
   return res;
 };
 
 export const getAuctionById = async (id: string) => {
-  const res = await axios.get(`${baseUrl}/get-auction-by-id/${id}`);
+  const res = await axios.get(`${baseUrl}/auctions/get-auction-by-id/${id}`);
   return res;
 };
 
 export const deleteAuction = async (id: string) => {
-  const res = await axios.delete(`${baseUrl}/delete-auction/${id}`);
+  const res = await axios.delete(`${baseUrl}/auctions/delete-auction/${id}`);
   return res;
 };
 
-export const createBid = async (body: any) => {
-  const res = await axios.post(`${baseUrl}/create-bid`, body);
+export const createBid = async (body: never) => {
+  const res = await axios.post(`${baseUrl}/bids/create-bid`, body);
   return res;
 };
 
-export const editUser = async (id: string, body: any) => {
-  const res = await axios.put(`${baseUrl}/edit-user/${id}`, body);
+export const editUser = async (id: string, body: never) => {
+  const res = await axios.put(`${baseUrl}/users/edit-user/${id}`, body);
   return res;
 };
